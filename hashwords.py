369d53356749705b978f9a3ab3a80a1ab3d73330
t3knomanser@gmail.com <remyporter@jetpackshark.com>
2014-08-25 20:02:59 -0400

    Added a mapped hash version.

--- hashwords.py
+++ hashwords.py
@@ -1,5 +1,23 @@
 import linecache
 import math
+from mmap import mmap
+
+def mappedhash(hash, wordlist="all.padded", address_size=4, line_size=15, character_size=2):
+	words = []
+	f = open(wordlist, "r+b")
+	mapped = mmap(f.fileno(), 0)
+	for i in range(0, len(hash), address_size):
+		hx = "00"
+		try:
+			hx = hash[i:i+address_size]
+		except IndexError as err:
+			hx = hash[-address_size:0]
+
+		line = int(hx, 16)
+		address = line_size * character_size * line-line_size*character_size
+		word = mapped[address:address+line_size*character_size]
+		words += [word.decode("UTF" + str(character_size*8)).strip()]
+	return " ".join(words)
 
 def wordhash(hash, wordlist="all.short", address_size=4):
 	words = []
