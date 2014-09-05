6eeb49d09d7272545b3b8bc13943bb64d8a315a1
t3knomanser@gmail.com <remyporter@jetpackshark.com>
2014-09-05 16:59:23 -0400

    Moved the markdown module around, also cleaned up functionality.

--- bundle/markdown.py
+++ bundle/markdown.py
@@ -1,21 +1,27 @@
 from markdown.treeprocessors import Treeprocessor
-from markdown.util import etree
+from markdown.extensions import Extension
+from markdown.util import etree, AtomicString
 from markdown import markdownFromFile
-import resources
+import bundle.resources
 
-class StyleExtension(Treeprocessor):
-	def __init__(*args, **kwargs):
-		super().__init__(*args, **kwargs)
+class StyleHelper(Treeprocessor):
+	def __init__(self, md, sheets):
+		super().__init__(md)
+		self.sheets = sheets
 
 	def run(self, root):
-		sheets = self.config["sheets"][0].split(",")
+		sheets = self.sheets.split(",")
 		for sheet in sheets:
-			elem = etree.Element("link")
-			elem.set("rel", "stylesheet")
-			elem.set("type", "text/css")
-			elem.set("href=", sheet)
+			css = bundle.resources.get_style(sheet)
+			elem = etree.Element("style")
+			elem.text = AtomicString(css)
 			root.insert(0,elem)
 		return elem
+
+class Style(Extension):
+	def extendMarkdown(self, md, md_globals):
+		style = StyleHelper(md, self.config["sheets"][0])
+		md.treeprocessors.add('stylesheets', style, "_end")
 
 class MarkdownProcessor:
 	def __init__(self, bundle):
@@ -29,7 +35,7 @@ class MarkdownProcessor:
 	def markdown(self, input_path, extensions=None):
 		exts = ['abbr','fenced_code','footnotes','tables','codehilite','smarty','toc', 'attr_list','def_list'] if extensions==None else extensions
 		style = ",".join(self.__bundle.stylesheets)
-		styleext = markdownstyle.StyleExtension(configs={"sheets":[style, "The stylesheet list"]})
+		styleext = Style(configs={"sheets":[style, "The stylesheet list"]})
 		exts += [styleext]
 		markdownFromFile(input_path, input_path + ".html", output_format="html5", extensions=extensions if exts==None else exts)
 
