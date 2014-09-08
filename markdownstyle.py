09c8aa83b2a38c71ab6e3dc578e7efd3e2651922
t3knomanser@gmail.com <remyporter@jetpackshark.com>
2014-09-08 13:39:54 -0400

    Implemented trivial case of image handling.

--- markdownstyle.py
+++ markdownstyle.py
@@ -2,6 +2,7 @@ from markdown.treeprocessors import Treeprocessor
 from markdown.extensions import Extension
 from markdown.util import etree, AtomicString
 from markdown import Markdown
+from markdown.inlinepatterns import Pattern
 import bundle.resources
 
 class StyleHelper(Treeprocessor):
@@ -18,19 +19,31 @@ class StyleHelper(Treeprocessor):
 			root.insert(0,elem)
 		return root
 
-class Style(Extension):
+class ImagePattern(Pattern):
+	def handleMatch(self, m):
+		img = etree.Element("img")
+		print(m.groups())
+		path = m.groups()[1]
+		data = bundle.resources.datauri(path, bundle.resources.get_resource(path))
+		img.set("src", data)
+		return img
+
+
+class MdStyle(Extension):
 	def extendMarkdown(self, md, md_globals):
 		style = StyleHelper(md, self.config["sheets"][0])
 		md.treeprocessors.add('stylesheets', style, "_end")
+		images = ImagePattern("\[\$img \"(.+?)\"(.*)\]")
+		md.inlinePatterns.add('images', images, "_begin")
 
 class MarkdownProcessor:
-	def __init__(self, stylesheets):
+	def __init__(self, stylesheets=[]):
 		self.__sheets = stylesheets
 
 	def markdown(self, input_path, extensions=None):
 		exts = ['abbr','fenced_code','footnotes','tables','codehilite','smarty','toc', 'attr_list','def_list'] if extensions==None else extensions
 		style = ",".join(self.__sheets)
-		styleext = Style(configs={"sheets":[style, "The stylesheet list"]})
+		styleext = MdStyle(configs={"sheets":[style, "The stylesheet list"]})
 		exts += [styleext]
 		md = Markdown(extensions=exts, output_format="html5")
 		md.stripTopLevelTags = False
